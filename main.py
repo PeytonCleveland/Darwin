03391ae057860644abd9d7d1f196927bdd18b679
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-09-05 22:05:08 -0500

    working prototype

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 import openai
 import csv
 import argparse
+from tqdm import tqdm
+from prompts import EQUAL_PROMPT, SYSTEM_PROMPTS, DEPTH_EVOLUTION_PROMPTS, DIFFICULTY_PROMPT, BREADTH_EVOLUTION_PROMPT
 
 # Load environment variables from .env.local file
-load_dotenv(dotenv_path=".env.local")
+load_dotenv()
 
 # Fetch the OpenAI API key from the environment variable
 openai_api_key = os.environ.get('OPENAI_API_KEY')
@@ -14,52 +16,31 @@ if not openai_api_key:
 
 openai.api_key = openai_api_key
 
-EQUAL_PROMPT="""Here are two Instructions to ChatGPT AI, do you think they are equal to each other, which meet the
-following requirements:
-
-1. They have same constraints and requirments.
-2. They have same depth and breadth of the inquiry.
-The First Prompt: {first}
-The Second Prompt: {second}
-Your Judgement (Just answer: Equal or Not Equal. No need to explain the reason.):"""
-
 class Node:
-    def __init__(self, prompt, response, is_refusal=False, parent=None):
-        self.prompt = prompt
-        self.response = response
+    def __init__(self, system_prompt, user_prompt, assistant_response, is_refusal=False, parent=None):
+        self.system_prompt = system_prompt
+        self.user_prompt = user_prompt
+        self.assistant_response = assistant_response
         self.is_refusal = is_refusal
         self.children = []
         self.parent = parent
 
-    def remove(self):
-        if self.parent:
-            self.parent.children.remove(self)
-        self.parent = None
-        self.children = []
-
-    def depth_evolve(self):
-        """Evolve node prompt by deepening, increasing reasoning, concretizing, adding constraints, or complicating input"""
-        pass
-
-    def breadth_evolve(self):
-        pass
-
-def should_prune(node, parent):
-    """Evaluate the fitness of a node"""
+def should_prune(evolved_prompt, parent):
+    """Evaluate the fitness of a potential child node based on its evolved prompt"""
 
     # Redundancy check
-    if node.prompt.strip == parent.prompt.strip:
+    if evolved_prompt.strip() == parent.user_prompt.strip():
         return True
 
-    # Prune if the node's prompt content is empty or just whitespace
-    if not node.prompt.strip():
+    # Prune if the evolved prompt content is empty or just whitespace
+    if not evolved_prompt.strip():
         return True
     
-    prompt = EQUAL_PROMPT.format(first=node.prompt, second=parent.prompt)
-    response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=50)
+    comparison_prompt = EQUAL_PROMPT.format(first=evolved_prompt, second=parent.user_prompt)
+    response = get_response(comparison_prompt)
 
     # Prune if the model deems them equal
-    if response.choices[0].text.strip() == "Equal":
+    if response.strip() == "Equal":
         return True
     
     return False
@@ -69,10 +50,19 @@ def load_forest_from_csv(filename):
     forest = []
     with open(filename, 'r') as file:
         reader = csv.reader(file)
+        
+        # Skip the header row
+        next(reader, None)
+        
         for row in reader:
-            prompt_text = row[0]  # assuming the prompt is in the first column
-            root_node = Node(Prompt(prompt_text))
+            is_refusal = row[0]
+            system_prompt = row[1]
+            user_prompt = row[2]
+            assistant_response = row[3]
+            root_node = Node(system_prompt, user_prompt, assistant_response, is_refusal)
             forest.append(root_node)
+
+    print("Loaded " + str(len(forest)) + " rows from " + filename)
     return forest
 
 def validate_csv_format(filename):
@@ -83,7 +73,7 @@ def validate_csv_format(filename):
         
         # 1. Validate header columns
         headers = reader.fieldnames
-        required_columns = {'prompt', 'response', 'is_refusal'}
+        required_columns = {'User', 'Assistant', 'Refusal'}
         
         if not required_columns.issubset(headers):
             print("Error: CSV is missing one or more required columns.")
@@ -98,28 +88,73 @@ def validate_csv_format(filename):
                     print(f"Error: Missing value in column '{column}' on row {row_num}.")
                     return False
             
-            if row['is_refusal'] not in valid_is_refusal_values:
-                print(f"Error: Invalid value '{row['is_refusal']}' in 'is_refusal' column for prompt '{row['prompt']}' on row {row_num}.")
+            if row['Refusal'] not in valid_is_refusal_values:
+                print(f"Error: Invalid value '{row['refusal']}' in 'refusal' column for prompt '{row['prompt']}' on row {row_num}.")
                 return False
 
     return True
 
 def get_response(prompt_text):
-    response = openai.Completion.create(engine="davinci", prompt=prompt_text, max_tokens=150)
-    return response.choices[0].text.strip()
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt_text}], temperature=0.8)
+    return response.choices[0].message.content
 
 def evolve(node):
-    # Use any of your evolution logic here.
-    evolved_prompt_text = get_response(node.prompt.text)  # Simplified evolution for demonstration
-    evolved_prompt = Prompt(evolved_prompt_text)
-    evolved_node = Node(evolved_prompt, parent=node)
-    node.children.append(evolved_node)
-    return evolved_node
+    """Evolve the current node, return node to add as a new root node"""
+
+    # Evolve node using depth evolution prompts
+    for prompt_template in DEPTH_EVOLUTION_PROMPTS:
+        prompt_text = prompt_template.format(prompt=node.user_prompt)
+        response_text = get_response(prompt_text)  # Assuming you have a function `get_response` that talks to OpenAI API
+        
+        # Check if the evolved prompt should be pruned
+        if not should_prune(response_text, node):
+            # If not pruned, then create a new child node and add to the tree
+            new_node = Node(system_prompt=node.system_prompt, user_prompt=response_text, assistant_response="", parent=node)
+            node.children.append(new_node)
+
+    # Evolve node using breadth evolution prompt
+    prompt_text = BREADTH_EVOLUTION_PROMPT.format(prompt=node.user_prompt)
+    response_text = get_response(prompt_text)
+    
+    # Check if the evolved prompt should be pruned
+    if not should_prune(response_text, node):
+        # If not pruned, then create a new child node and add to the tree
+        new_node = Node(system_prompt=node.system_prompt, user_prompt=response_text, assistant_response="", parent=node)
+        return new_node
+    
+
+def get_leaf_nodes(node):
+    """Recursively get all leaf nodes."""
+    if not node.children:
+        return [node]
+    leaves = []
+    for child in node.children:
+        leaves.extend(get_leaf_nodes(child))
+    return leaves
 
 def print_tree(node, indent=""):
-    print(indent + str(node.prompt))
+    print(indent + str(node.user_prompt))
     for child in node.children:
         print_tree(child, indent + "  ")
+
+def forest_stats(forest):
+    """
+    Calculate and return the number of trees and total nodes in the forest.
+    """
+    trees = len(forest)  # Each root node in the forest is a tree.
+    
+    def count_nodes(node):
+        """
+        Recursively count the number of nodes in the subtree rooted at node.
+        """
+        total = 1  # Include the current node.
+        for child in node.children:
+            total += count_nodes(child)
+        return total
+    
+    nodes = sum(count_nodes(root) for root in forest)
+
+    return trees, nodes
 
 def main():
     parser = argparse.ArgumentParser(description='Evolve prompts using GPT-4 based on a CSV input.')
@@ -129,7 +164,12 @@ def main():
     parser.add_argument('output_file', type=str, nargs='?', default='output.csv', 
                         help='Path to the output file where results will be saved. Defaults to "output.csv".')
     
+    # Option to validate the input csv and exit
     parser.add_argument('--validate', action='store_true', help='Check if the CSV is in the correct format and then exit.')
+
+    # Optional argument for the number of epochs, defaults to 4
+    parser.add_argument('--epochs', type=int, default=4, help='Number of epochs to evolve the leaf nodes. Defaults to 4.')
+
 
     args = parser.parse_args()
     
@@ -141,6 +181,34 @@ def main():
         return  # Exit after validation
     
     forest = load_forest_from_csv(args.csv_file)
+
+    print("Evolving prompts from " + args.csv_file + " for " + str(args.epochs) + " epochs.")
+
+    # Evolving for the specified number of epochs
+    for epoch in range(args.epochs):
+        print('Evolving epoch #' + str(epoch + 1))
+        all_leaf_nodes = []
+        for root in forest:
+            all_leaf_nodes.extend(get_leaf_nodes(root))
+        
+        leaves_added = 0
+        root_nodes_added = 0
+        
+        # Use tqdm to wrap around the leaf nodes iteration
+        for leaf in tqdm(all_leaf_nodes, desc=f"Epoch {epoch+1} Progress", ncols=100):
+            new_root = evolve(leaf)
+            if new_root:
+                forest.append(new_root)
+                root_nodes_added += 1
+            leaves_added += len(leaf.children)
+        
+        # Print stats of the current forest and the added leaves and root nodes
+        trees, nodes = forest_stats(forest)
+        print(f"\nTotal Trees in Forest: {trees}, Total Nodes in Forest: {nodes}")
+        print(f"Leaves Added in Epoch {epoch+1}: {leaves_added}")
+        print(f"Root Nodes Added in Epoch {epoch+1}: {root_nodes_added}")
+
+
 
 if __name__ == "__main__":
     main()
---
+++ prompts.py
@@ -0,0 +1,86 @@
+EQUAL_PROMPT="""Here are two Instructions to ChatGPT, do you think they are equal to each other? Equal prompts
+meet the following requirements:
+
+1. They have same constraints and requirments.
+2. They have same depth and breadth of the inquiry.
+3. They have interchanged words or are rephrased but don't have meaningful differences otherwise
+
+The First Prompt: {first}
+The Second Prompt: {second}
+Your Judgement (Just answer: Equal or Not Equal. No need to explain the reason.):"""
+
+SYSTEM_PROMPTS=[
+    # 1
+    """You are Ocelot, a large language model trained by Digital University, based on the Llama 2 architecture. 
+    You are helpful, respectful, and honest. Always answer as helpfully as possible, while being safe. If a 
+    question does not make sense, or is not factually coherent, explain why instead of answering with something
+    not correct. Your training cutoff date is 2023-07. The current date is {date}.""",
+    # 2
+    """You are a helpful assistant. Respond truthfully and think through your instructions step-by-step.""",
+    # 3
+    """You are an AI assistant that helps people find information. User will you give you a question. Your task
+    is to answer as faithfully as you can. While answering think step-bystep and justify your answer."""
+]
+
+DEPTH_EVOLUTION_PROMPTS=[
+    # 1. Deepening
+    """I want you act as a Prompt Rewriter. Your objective is to rewrite a given prompt into a more complex version
+    to train future language models on realistic, complext tasks. The rewritten prompt must be reasonable, must sound
+    like it's written by a curious user and must be understandable. Do not simply reword the existing prompt. Rather,
+    add depth by introducing more layers or aspects to the subject Your rewriting cannot omit the non-text parts such
+    as any table or code in #Given Prompt#:. You SHOULD complicate the given prompt using the following method: If
+    #Given Prompt# contains inquiries about certain issues, the depth and breadth of the inquiry can be increased.
+    ‘given prompt’ and ‘rewritten prompt’ are not allowed to appear in #Rewritten Prompt#, only respond with the
+    rewritten prompt.
+    #Given Prompt#:
+    {prompt}
+    #Rewritten Prompt#:""",
+
+    # 2. Concretizing
+    """I want you act as a Prompt Rewriter. Your objective is to rewrite a given prompt into a more complex version
+    to train future language models on realistic, complex tasks. The rewritten prompt must be reasonable, must sound
+    like it's written by a curious user and must be understandable. Your rewriting cannot omit the non-text parts such
+    as any table or code in #Given Prompt#:. You SHOULD complicate the given prompt using the following method: Please
+    replace general concepts with more specific concepts. ‘given prompt’ and ‘rewritten prompt’ are not allowed to appear
+    in #Rewritten Prompt#, only respond with the rewritten prompt.
+    #Given Prompt#:
+    {prompt}
+    #Rewritten Prompt#:""",
+
+    # 3. Increasing Reasoning
+    """I want you to act as a Prompt Rewriter. Your objective is to rewrite a given prompt to require more detailed reasoning 
+    and thoughtful answers from future language models. The rewritten prompt must be reasonable, must sound like it's written
+    by a curious user and must be understandable. Your rewriting cannot omit the non-text parts such as any table or code in
+    #Given Prompt#. You SHOULD complicate the given prompt by asking for more in-depth reasoning or requiring multiple steps
+    of thought. ‘given prompt’ and ‘rewritten prompt’ are not allowed to appear in #Rewritten Prompt#, only respond with the
+    rewritten prompt.
+    #Given Prompt#:
+    {prompt}
+    #Rewritten Prompt#:""",
+    
+    # 4. Complicating Input
+    """I want you to act as a Prompt Rewriter. Imagine you are crafting a question that demands more intellectual engagement.
+    The rewritten prompt must be reasonable, must sound like it's written by a curious user, and be understandable. Your
+    rewriting cannot omit the non-text parts such as any table or code in #Given Prompt#. You SHOULD complicate the given
+    prompt by asking for more in-depth reasoning or requiring multiple steps of thought. ‘given prompt’ and ‘rewritten prompt’
+    are not allowed to appear in #Rewritten Prompt#. Only respond with the rewritten prompt.
+    #Given Prompt#:
+    {prompt}
+    #Rewritten Prompt#:"""
+]
+
+DIFFICULTY_PROMPT="""We would like you to evaluate and rate the difficulty and complexity of the following prompt. You
+should give an overall score on a scale of 1 to 10, where a higher score indicates higher difficulty and
+complexity. You must just give a score without any other reasons.
+## Prompt:
+{prompt}
+## Score:"""
+
+BREADTH_EVOLUTION_PROMPT="""I want you to act as a Prompt Creator. Your goal is to draw inspiration from the #Given Prompt#
+to create a brand new prompt. This new prompt should belong to the same domain as the #Given Prompt# but be even more unique. 
+The LENGTH and difficulty level of the #Created Prompt# should be similar to that of the #Given Prompt#. The #Created Prompt#
+must be reasonable, must sound like it's written by a curious user and must be understood and responded to by humans. ‘given prompt’
+and ‘created prompt’ are not allowed to appear in #Created Prompt#.
+#Given Prompt#:
+{prompt}
+#Created Prompt#:"""
